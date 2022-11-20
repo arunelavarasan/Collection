349a3b8a36a733a35733ff8eb6f67906af63eb2c
arunelavarasan <arunelavarasan168@gmail.com>
2022-11-20 23:13:20 +0530

    List

---
+++ List/priorityQueue_Example.java
@@ -0,0 +1,19 @@
+package org.jsp.collection.queue;
+
+import java.util.PriorityQueue;
+
+public class priorityQueue_Example {
+	
+	public static void main(String[] args) {
+		//Queue allows homogeneous type of object
+		PriorityQueue<Object> p = new PriorityQueue<>();
+		p.add("kldj");
+		p.add("lkjasd");
+		p.add("jdslj");
+		p.add("lksdj");
+		System.out.println(p);
+		System.out.println(p.peek());
+		System.out.println(p.poll());
+		System.out.println(p);
+	}
+}
